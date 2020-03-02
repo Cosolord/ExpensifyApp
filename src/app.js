d94b6e9d5dd484d4863017b4ee681518b0fdbd2d
CosoLord <cosolord@gmail.com>
2020-03-02 02:07:03 +0100

    rimosse le spese inserite hard coded in app.js

--- src/app.js
+++ src/app.js
@@ -10,15 +10,6 @@ import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
 const store = configureStore();
-/* 
-store.dispatch(addExpense({ description: 'Water bill', amount: 122, createdAt: 11 }));
-store.dispatch(addExpense({ description: 'Gas bill', amount: 359, createdAt: 1000 }));
-store.dispatch(addExpense({ description: 'Internet bill', amount: 75, createdAt: 2267456743534 }));
-
-const state = store.getState();
-console.log(state);
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-console.log(visibleExpenses); */
 
 const jsx = (
   <Provider store={store}>
